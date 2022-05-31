9241211566b036e5550a91dbe3cce6785eba91e2
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-31 14:33:43 +0200

    create welcome home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :categories do
-    resources :payments
+  devise_scope :user do
+    authenticated :user do
+      root 'categories#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'users#welcome', as: :unauthenticated_root
+    end
   end
+  resources :categories
+  resources :payments
+  
   root "categories#index"
 end
