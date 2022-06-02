1625084c5b5377de10df2c4b77a15cda43f3dd40
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-06-02 13:23:10 +0200

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name:      Rails.application.credentials.dig(:gmail_smtp, :email),
     password:       Rails.application.credentials.dig(:gmail_smtp, :password),
-    domain:         'cspent.herokuapp.com',
+    domain:         'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
